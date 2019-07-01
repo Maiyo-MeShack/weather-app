0d9b5fa3c5b1cc5d40d2c77cd37abf75fb0ab48f
S3AN <35777299+Maiyo-MeShack@users.noreply.github.com>
2019-07-01 00:55:37 -0700

    Add files via upload

---
+++ src/main/java/com/example/enrico/complexforms/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.enrico.complexforms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editName);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Toast.makeText(getApplicationContext(),v.getText(), Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
